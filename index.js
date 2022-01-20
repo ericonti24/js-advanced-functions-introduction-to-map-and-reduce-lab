beb083590b535a51a0dcea14bee4aab923af48ee
Eric <ericontiveros123@gmail.com>
2022-01-19 21:39:21 -0700

    commit tree a3fab785e62bcfa6f59ea6448b80b100e8778204

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Your code here
+function mapToNegativize(array) {
+   return array.map(x => x * -1);
+};
+
+function mapToNoChange(array) {
+    return array;
+}
+
+function mapToDouble(array) {
+    return array.map(x => x * 2)
+}
+
+function mapToSquare(array) {
+    return array.map(x => x ** 2)
+}
+
+function reduceToTotal(array, startPoint = 0) {
+    let total = startPoint
+    for (let i = 0; i < array.length; i++) {
+        total = total + array[i]
+    }
+    return total
+}
+
+function reduceToAllTrue(array) {
+    for (let i = 0; i < array.length; i++) {
+        if (!array[i]) 
+        return false
+    }
+    return true
+}
+
+function reduceToAnyTrue(array) {
+    for (let i = 0; i < array.length; i++) {
+        if (array[i])
+        return true
+    }
+    return false
+}
